1effd0f7611738c32bd9ed4ff7fc475ae570eddb
2pac <tarasyaremabcn@gmail.com>
2025-11-18 01:19:16 +0100

    desplega tunnel

--- bin/lt.js
+++ bin/lt.js
@@ -17,7 +17,7 @@ const { argv } = yargs
   .option('h', {
     alias: 'host',
     describe: 'Upstream server providing forwarding',
-    default: 'https://localtunnel.me',
+    default: 'https://lt.desplega.ai',
   })
   .option('s', {
     alias: 'subdomain',
